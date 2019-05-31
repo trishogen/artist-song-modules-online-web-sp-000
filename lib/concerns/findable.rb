257999b97f9ef517778369e479202126fe9f3257
Trish Ogen <patriciaogen@gmail.com>
2019-05-31 21:46:18 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,7 @@
 module Findable
 
   def find_by_name(name)
-    ObjectSpace.each_object(all).detect{|a| a.name == name}
+    ObjectSpace.each_object(self.all).detect{|a| a.name == name}
   end
 
 end
