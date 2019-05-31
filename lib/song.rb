320f4379d1fc06ab7323ca71e281f44c71fc2d3f
Trish Ogen <patriciaogen@gmail.com>
2019-05-31 21:50:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Song
   extend Memorable
   extend Findable
+  include Paramable
 
   attr_accessor :name
   attr_reader :artist
@@ -21,7 +22,7 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
